367b301755ac1d54f0d3204471e50ed39d4cc871
ChiquiPR <johansteven.h10@gmail.com>
2022-11-26 22:39:03 -0500

    class views

--- polls/views.py
+++ polls/views.py
@@ -2,25 +2,44 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, 'polls/index.html', {
-        'latest_question_list' : latest_question_list
-    })
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request, 'polls/index.html', {
+#         'latest_question_list' : latest_question_list
+#     })
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/details.html', {
-        'question' : question
-    })
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/details.html', {
+#         'question' : question
+#     })
 
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {
-        'question' : question
-    })
+# def result(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {
+#         'question' : question
+#     })
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        "return the last 5 published questions"
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def votes(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
